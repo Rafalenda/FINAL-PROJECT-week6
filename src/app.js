ef6c30beff4568f86e95ce680b3cc4c447bdf2ec
Rafaela Almeida Da Silva <rafaela.almeida.silva@gmail.com>
2020-09-03 19:05:42 -0400

    Update the time

--- src/app.js
+++ src/app.js
@@ -1,4 +1,28 @@
-function displayTemperature(response) {
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  return `${day} ${hours}:${minutes}`;
+}
+
+function displayWeatherData(response) {
   console.log(response.data);
 
   let cityElement = document.querySelector("#city");
@@ -15,11 +39,12 @@ function displayTemperature(response) {
 
   let windElement = document.querySelector("#wind-speed");
   windElement.innerHTML = Math.round(response.data.wind.speed);
+
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "cf18efdcc3ab33ec3dbe62988aef1ff5";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Toronto&appid=${apiKey}&units=metric`;
 
-axios.get(apiUrl).then(displayTemperature);
-
-console.log(new Date());
+axios.get(apiUrl).then(displayWeatherData);
