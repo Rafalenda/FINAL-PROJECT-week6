c72c6ab9a02fec68dad812905c62f5eec74ef326
Rafaela Almeida Da Silva <rafaela.almeida.silva@gmail.com>
2020-09-03 18:06:50 -0400

    Add funcionality, data from API

---
+++ src/app.js
@@ -0,0 +1,23 @@
+function displayTemperature(response) {
+  console.log(response.data);
+
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+
+  let windElement = document.querySelector("#wind-speed");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+apiKey = "cf18efdcc3ab33ec3dbe62988aef1ff5";
+apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Toronto&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
